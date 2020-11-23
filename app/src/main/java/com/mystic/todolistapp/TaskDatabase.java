9f5960b1b683bc5546837af026108c14aa8edcb7
mystic001 <bamideleomonayin@gmail.com>
2020-11-23 20:39:01 +0100

    removing the task we hardcoded

--- app/src/main/java/com/mystic/todolistapp/TaskDatabase.java
+++ app/src/main/java/com/mystic/todolistapp/TaskDatabase.java
@@ -18,35 +18,9 @@ public abstract class TaskDatabase extends RoomDatabase {
     public static synchronized TaskDatabase getTaskDatabaseInstance(Context context){
         if(taskDatabaseInstance == null){
             taskDatabaseInstance = Room.databaseBuilder(context, TaskDatabase.class, "task_database")
-                    .addCallback(roomCallback)
                     .build();
         }
         return taskDatabaseInstance;
-    }
-
-    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
-        @Override
-        public void onCreate(@NonNull SupportSQLiteDatabase db) {
-            super.onCreate(db);
-            new PopulateBase(taskDatabaseInstance).execute();
-        }
-    };
-
-    private static class PopulateBase extends AsyncTask<Void,Void,Void>{
-        TaskDao taskDao;
-
-        public  PopulateBase(TaskDatabase db){
-            taskDao = db.taskDao();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            Task task = new Task();
-            task.setTitle("Task1");
-            task.setDate("12/03/2020");
-            taskDao.insert(task);
-            return null;
-        }
     }
 
 
