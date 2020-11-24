26b884a1b6015bca6256b6803a4df5ad8fc9c855
mystic001 <bamideleomonayin@gmail.com>
2020-11-24 07:15:05 +0100

    picture intent was made active, also we prevented the selection of date in the past

--- app/src/main/java/com/mystic/todolistapp/CreateTaskActivity.java
+++ app/src/main/java/com/mystic/todolistapp/CreateTaskActivity.java
@@ -1,18 +1,29 @@
 package com.mystic.todolistapp;
 
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.DialogFragment;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -25,6 +36,9 @@ public class CreateTaskActivity extends AppCompatActivity implements DatePickerD
     private Button btnAdd ;
     private Button btn_setDate;
     private Task mtask = new Task();
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView imageView;
+    private String currentPhotoPath;
 
 
 
@@ -35,6 +49,7 @@ public class CreateTaskActivity extends AppCompatActivity implements DatePickerD
 
 
         mTitleField = findViewById(R.id.title);
+        imageView = findViewById(R.id.imgView);
         setTime = findViewById(R.id.timeseter);
         btnAdd = findViewById(R.id.btn_add);
         btn_setDate = findViewById(R.id.btn_date);
@@ -90,8 +105,9 @@ public class CreateTaskActivity extends AppCompatActivity implements DatePickerD
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.timeseter:
-                TimePickerDialog dialog = new TimePickerDialog();
-                showPopUp(dialog,DIALOG_TIME);
+                dispatchTakePictureIntent();
+                //TimePickerDialog dialog = new TimePickerDialog();
+                //showPopUp(dialog,DIALOG_TIME);
                 break;
             case R.id.btn_date:
                 DatePickerDialog dialog_date = new DatePickerDialog();
@@ -108,5 +124,63 @@ public class CreateTaskActivity extends AppCompatActivity implements DatePickerD
     private void showPopUp(DialogFragment frag, String str) {
         frag.show(getSupportFragmentManager(),str);
     }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+
+            // Continue only if the File was successfully created
+
+            if(photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.mystic.todolistapp.fileprovider",
+                        photoFile);
+                try {
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                } catch (ActivityNotFoundException e) {
+                    // display error state to the user
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            assert data != null;
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+
 
 }
