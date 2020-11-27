6739f3126af4ec08432898dba5e49da644dc3d8b
mystic001 <bamideleomonayin@gmail.com>
2020-11-27 01:08:38 +0100

    adjusting my picture margin

--- app/src/main/java/com/mystic/todolistapp/TaskAdapter.java
+++ app/src/main/java/com/mystic/todolistapp/TaskAdapter.java
@@ -72,14 +72,14 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskHolder>  {
                 .update(mBindTask);*/
 
 
-        /*if(holder.imag != null){
+        if(mBindTask.getImage() != null){
 
             Glide.with(context)
                     .asBitmap()
                     .circleCrop()
                     .load(Uri.parse(mBindTask.getImage()))
                     .into(holder.imag);
-        }*/
+        }
 
 
 
